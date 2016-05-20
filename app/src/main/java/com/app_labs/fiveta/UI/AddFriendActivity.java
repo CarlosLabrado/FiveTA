843ad62ebf374e5c93a4364a586ee236bdbb1ba7
Carlos Labrado <labrado.carlos@gmail.com>
2016-05-19 18:06:01 -0600

    Feat: Add friends functionality

---
+++ app/src/main/java/com/app_labs/fiveta/UI/AddFriendActivity.java
@@ -0,0 +1,179 @@
+package com.app_labs.fiveta.ui;
+
+import android.os.Bundle;
+import android.support.annotation.MainThread;
+import android.support.annotation.StringRes;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+import com.app_labs.fiveta.R;
+import com.app_labs.fiveta.model.User;
+import com.app_labs.fiveta.util.Utils;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class AddFriendActivity extends AppCompatActivity {
+
+    @Bind(R.id.toolbar)
+    Toolbar mToolbar;
+    @Bind(R.id.friendLists)
+    RecyclerView mFriendListsRecyclerView;
+    @Bind(R.id.addFriendRootView)
+    CoordinatorLayout mRootView;
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference mRef;
+    private Query mUsersRef;
+
+    private LinearLayoutManager mManager;
+    private FirebaseRecyclerAdapter<User, UserHolder> mRecyclerViewAdapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_friend);
+
+        ButterKnife.bind(this);
+        /** toolBar **/
+        setUpToolBar();
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mRef = FirebaseDatabase.getInstance().getReference();
+        mUsersRef = mRef.child("users");
+
+        populateRecyclerAdapter();
+    }
+
+    private void populateRecyclerAdapter() {
+        mManager = new LinearLayoutManager(this);
+        mFriendListsRecyclerView.setLayoutManager(mManager);
+
+        mRecyclerViewAdapter = new FirebaseRecyclerAdapter<User, UserHolder>(
+                User.class, R.layout.item_users, UserHolder.class, mUsersRef) {
+
+            @Override
+            protected void populateViewHolder(UserHolder viewHolder, final User model, int position) {
+                viewHolder.setName(model.getName());
+                viewHolder.setEmail(model.getEmail());
+
+                viewHolder.mView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String selectedUserKey = Utils.encodeEmail(model.getEmail());
+                        createFriendRelationship(selectedUserKey);
+
+                    }
+                });
+
+            }
+        };
+
+        mFriendListsRecyclerView.setAdapter(mRecyclerViewAdapter);
+    }
+
+    /**
+     * Firebase Magic, adds the encoded email as a key in friends
+     *
+     * @param selectedUserKey clicked friend
+     */
+    private void createFriendRelationship(String selectedUserKey) {
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        DatabaseReference friendsReference = FirebaseDatabase.getInstance().getReference("friends");
+
+        if (currentUser != null) {
+            String currentLoggedUser = Utils.encodeEmail(currentUser.getEmail());
+            if (selectedUserKey.equalsIgnoreCase(currentLoggedUser)) {
+                showSnackBar(R.string.error_cant_friend_yourself);
+            } else {
+                friendsReference.child(currentLoggedUser).child(selectedUserKey).setValue(true);
+                //originalUserReference.child(Constants.FIREBASE_LOCATION_USERS).child(currentLoggedUser).child("friends").child(selectedUserKey).setValue(true);
+                showSnackBar(R.string.snack_friend_added);
+                finish();
+            }
+        }
+    }
+
+    /**
+     * sets up the top bar
+     */
+    public void setUpToolBar() {
+        setSupportActionBar(mToolbar);
+        setActionBarTitle(getString(R.string.friends_add_toolbar), null, true);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayShowTitleEnabled(true);
+            // enabling action bar app icon and behaving it as toggle button
+            getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeButtonEnabled(true);
+        }
+    }
+
+    /**
+     * Gets called from the fragments onResume and its because only the first doesn't have the up
+     * button on the actionBar
+     *
+     * @param title          The title to show on the ActionBar
+     * @param subtitle       The subtitle to show on the ActionBar
+     * @param showNavigateUp if true, shows the up button
+     */
+    public void setActionBarTitle(String title, String subtitle, boolean showNavigateUp) {
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(title);
+            if (subtitle != null) {
+                getSupportActionBar().setSubtitle(subtitle);
+            } else {
+                getSupportActionBar().setSubtitle(null);
+            }
+            if (showNavigateUp) {
+                getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            } else {
+                getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+            }
+        }
+    }
+
+    public static class UserHolder extends RecyclerView.ViewHolder {
+        View mView;
+
+        public UserHolder(View itemView) {
+            super(itemView);
+            mView = itemView;
+            mView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                }
+            });
+        }
+
+        public void setName(String name) {
+            TextView field = (TextView) mView.findViewById(R.id.textViewUsersName);
+            field.setText(name);
+        }
+
+        public void setEmail(String text) {
+            TextView field = (TextView) mView.findViewById(R.id.textViewUsersEmail);
+            field.setText(text);
+        }
+    }
+
+    @MainThread
+    private void showSnackBar(@StringRes int errorMessageRes) {
+        Snackbar.make(mRootView, errorMessageRes, Snackbar.LENGTH_LONG)
+                .show();
+    }
+}
---
+++ app/src/main/java/com/app_labs/fiveta/UI/FriendsFragment.java
@@ -0,0 +1,184 @@
+package com.app_labs.fiveta.ui;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.app_labs.fiveta.R;
+import com.app_labs.fiveta.model.User;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link FriendsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FriendsFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    @Bind(R.id.friendLists)
+    RecyclerView mFriendListRecyclerView;
+    @Bind(R.id.fab_friend_add)
+    FloatingActionButton mFabFriendAdd;
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private DatabaseReference mRef;
+    private Query mUsersRef;
+
+    private FirebaseAuth mAuth;
+
+
+    private LinearLayoutManager mManager;
+    private FirebaseRecyclerAdapter<User, UserHolder> mRecyclerViewAdapter;
+
+
+    public FriendsFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment FriendsFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static FriendsFragment newInstance(String param1, String param2) {
+        FriendsFragment fragment = new FriendsFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_friends, container, false);
+        ButterKnife.bind(this, view);
+
+        mRef = FirebaseDatabase.getInstance().getReference();
+        mUsersRef = mRef.child("users");
+        mAuth = FirebaseAuth.getInstance();
+
+
+        populateRecyclerAdapter();
+
+        return view;
+    }
+
+    private void populateRecyclerAdapter() {
+//        mManager = new LinearLayoutManager(getContext());
+//        mFriendListRecyclerView.setLayoutManager(mManager);
+//
+//        FirebaseUser currentUser = mAuth.getCurrentUser();
+//        String currentLoggedUser = "";
+//        if (currentUser != null) {
+//            currentLoggedUser = Utils.encodeEmail(currentUser.getEmail());
+//        }
+//        mUsersRef = mRef.child("users").child(currentLoggedUser).child("friends").addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                FirebaseDatabase firebaseDatabase = mRef.child("users").child(dataSnapshot.getKey());
+//
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//
+//            }
+//        },"");
+
+//        mRecyclerViewAdapter = new FirebaseRecyclerAdapter<User, UserHolder>(
+//                User.class, R.layout.item_users, UserHolder.class, mUsersRef) {
+//
+//            @Override
+//            protected void populateViewHolder(UserHolder viewHolder, final User model, int position) {
+//                viewHolder.setName(model.getName());
+//                viewHolder.setEmail(model.getEmail());
+//
+//                viewHolder.mView.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        String selectedUserKey = Utils.encodeEmail(model.getEmail());
+////                        createFriendRelationship(selectedUserKey);
+//
+//                    }
+//                });
+//
+//            }
+//        };
+//
+//        mFriendListRecyclerView.setAdapter(mRecyclerViewAdapter);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
+
+    @OnClick(R.id.fab_friend_add)
+    public void onClick() {
+        Intent intent = new Intent(getActivity(), AddFriendActivity.class);
+        startActivity(intent);
+    }
+
+    public static class UserHolder extends RecyclerView.ViewHolder {
+        View mView;
+
+        public UserHolder(View itemView) {
+            super(itemView);
+            mView = itemView;
+            mView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                }
+            });
+        }
+
+        public void setName(String name) {
+            TextView field = (TextView) mView.findViewById(R.id.textViewUsersName);
+            field.setText(name);
+        }
+
+        public void setEmail(String text) {
+            TextView field = (TextView) mView.findViewById(R.id.textViewUsersEmail);
+            field.setText(text);
+        }
+    }
+}
